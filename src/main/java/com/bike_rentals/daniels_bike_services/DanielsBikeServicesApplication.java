ac1f8ff59eb8b70193674679328434823db55a8b
danielkpobari <danielkpobari@gmail.com>
2024-02-02 16:12:10 +0100

    first commit

---
+++ src/main/java/com/bike_rentals/daniels_bike_services/DanielsBikeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bike_rentals.daniels_bike_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielsBikeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielsBikeServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bike_rentals/daniels_bike_services/DanielsBikeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bike_rentals.daniels_bike_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielsBikeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
